40a954992c628257eb456fa3f3cb25dd0612264d
elikantor <eli.kantor@outlook.com>
2020-01-22 16:34:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+class Polygon {
+    constructor(array){
+        this.sides = array.length
+    }
+
+    get countSides(array){
+        return array.length
+    }
+
+    get perimeter(array){
+        let total = 0
+        array.forEach(ele => total += ele)
+        return total
+    }
+
+}
